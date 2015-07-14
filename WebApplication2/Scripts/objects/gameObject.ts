46983da510dc196dcf28c9379cc63f8ae44a0534
mohitagastya <mohitagastya4@gmail.com>
2015-07-14 12:08:01 -0400

    FINAL ASSIGNMENT

---
+++ WebApplication2/Scripts/objects/gameObject.ts
@@ -0,0 +1,28 @@
+﻿module objects {
+    // Gameobject Class ++++++++++++++++++++++++++++++++++++++
+    export class GameObject extends createjs.Bitmap {
+        // PUBLIC PROPERTIES ++++++++++++++++++++++++++++
+        public width: number;
+        public height: number;       
+        public isColliding: boolean = false;
+        public soundString: string = "";
+        //protected properties
+        protected dy: number;
+        protected dx: number;
+        public name: string = "";
+
+
+        // CONSTRUCTOR ++++++++++++++++++++++++++++++++++
+        constructor(imageString: string) {
+            super(imageString);
+            this.width = this.getBounds().width;
+            this.height = this.getBounds().height;
+            this.regX = this.width * 0.5;
+            this.regY = this.height * 0.5;
+
+        
+        }
+
+      
+    }
+}  
